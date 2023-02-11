34ff34fa80f6a226af75c36be05740d0d8d74f0a
Invincible-21 <shantobd1969@gmail.com>
2023-02-12 02:14:25 +0600

    updated

---
+++ 1805.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    
+    ll a,b; cin>>a>>b;
+    ll sum = (a+b)*(b-a+1)/2;
+
+    cout<<sum<<endl;
+    
+    return 0;
+}
+
