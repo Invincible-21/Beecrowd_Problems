125a58d3d77d942b87990def311ec9420bc238fd
Invincible-21 <shantobd1969@gmail.com>
2023-04-05 04:18:10 +0600

    updated

---
+++ 2787_Chess.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    
+    int r,c;
+    cin>>r>>c;
+
+    if((r+c)%2 != 0) cout<<'0'<<endl;
+    else cout<<'1'<<endl;
+    
+    return 0;
+}
+
