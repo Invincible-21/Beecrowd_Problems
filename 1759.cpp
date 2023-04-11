8aae16cd0772655582c1f7a845005484dbf06be2
Invincible-21 <shantobd1969@gmail.com>
2023-04-12 02:45:49 +0600

    ho ho ho

---
+++ 1759.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin >> n;
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << "Ho ";
+    }
+    cout << "Ho!\n";
+
+    return 0;
+}
