1bef48df379614b8c40174fe3b17c35b67daabbb
Invincible-21 <shantobd1969@gmail.com>
2024-01-21 20:43:39 +0600

    ..

--- 1273_Justifier.cpp
+++ 1273_Justifier.cpp
@@ -2,15 +2,31 @@
 using namespace std;
 
 int main(){
-    int n; cin >> n;
-    int sz = 0;
-    while(n--){
-        string s; cin >> s;
-        if(s.size() > sz) sz = s.size();
-    }
 
-    while(n--){
-        
+    while(1){
+        int n; cin >> n;
+        if(n == 0) return 0;
+
+        vector<string>v;
+    
+        int sz = -1;
+
+        while(n--){
+            string s; cin >> s;
+            int ss = s.size();
+            if(ss > sz) sz = ss;
+            v.push_back(s);
+        }
+
+        for(auto x: v){
+            int ss = x.size();
+            int y = sz - ss;
+            while(y--){
+                cout << ' ';
+            }
+            cout << x << '\n';
+        }
+        cout << '\n';
     }
 
     return 0;
